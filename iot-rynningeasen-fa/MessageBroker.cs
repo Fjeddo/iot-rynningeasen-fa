e9150689e0bda35fa8891ff326b944ca209e30e7
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-02-03 18:39:33 +0100

    Trying some stuff with config

--- iot-rynningeasen-fa/MessageBroker.cs
+++ iot-rynningeasen-fa/MessageBroker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -5,16 +6,15 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
-using System.Configuration;
 
 namespace IoTRynningeasenFA
 {
     public static class MessageBroker
     {
         [FunctionName("IoTMessageBroker")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req, TraceWriter log, ExecutionContext context)
         {
-            var configuration = ConfigurationManager.AppSettings;
+            var configuration = Environment.GetEnvironmentVariables();
 
             log.Info("C# HTTP IoT Messagebroker function processed a request.");
 
@@ -24,7 +24,7 @@ namespace IoTRynningeasenFA
             log.Info($"Request body: {requestBody}");
             log.Info($"Debug: {configuration["iot-www-api-location"]}");
 
-            var dummy = httpClient.PostAsync(configuration["iot-www-api-location"], new StringContent(requestBody, System.Text.Encoding.UTF8, "application/json")).Result;
+            var dummy = httpClient.PostAsync(configuration["iot-www-api-location"].ToString(), new StringContent(requestBody, System.Text.Encoding.UTF8, "application/json")).Result;
 
             dynamic[] data = JsonConvert.DeserializeObject<dynamic[]>(requestBody);
 
