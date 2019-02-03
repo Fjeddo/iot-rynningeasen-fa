dd39d2217c472df4bad23a0dcf30fe7ff42a70e3
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2019-02-03 14:05:31 +0100

    Ok, ready for action

---
+++ iot-rynningeasen-fa/MessageBroker.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+
+namespace IoTRynningeasenFA
+{
+    public static class MessageBroker
+    {
+        [FunctionName("IoTMessageBroker")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP IoT Messagebroker function processed a request.");
+
+            var httpClient = new HttpClient();
+
+            string requestBody = req.Content.ReadAsStringAsync().Result;
+            log.Info($"Request body: {requestBody}");
+
+            var dummy = httpClient.PostAsync("", new StringContent(requestBody, System.Text.Encoding.UTF8, "application/json")).Result;
+
+            dynamic[] data = JsonConvert.DeserializeObject<dynamic[]>(requestBody);
+
+            foreach (var @group in data)
+            {
+                var channelKey = ""; // 693480 - temp
+                if (@group.Name == "pressure")
+                {
+                    channelKey = ""; // 693482 - pressure
+                }
+
+                var fields = "";
+                foreach (var value in @group.Values)
+                {
+                    log.Info($"{value.Name}: {value.Value}");
+
+                    var @field = int.Parse(value.Name.Split(':')[1]) - 100;
+                    fields += $"&field{@field}=" + value.Value;
+                }
+
+                dummy = httpClient.GetAsync($"https://api.thingspeak.com/update?api_key={channelKey}{fields}").Result;
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK, $"Done");
+        }
+    }
+}
